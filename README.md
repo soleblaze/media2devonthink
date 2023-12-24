553dcfc34aa135a4c80ae062040c6125761bf4a6
Soleblaze <soleblaze@skyshadows.net>
2023-12-24 11:52:01 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# media2devonthink
+
+Creates transcripts and summaries of media, such as podcasts and video, and adds it to a devonthink database.
---
+++ m2dt/m2dt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""media2devonthink creates transcripts and summaries for media and adds it to a devonthink database."""
+
+def run():
+    """Runs the app"""
+    print("TODO")
+
+if __name__ == "__main__":
+    run()
